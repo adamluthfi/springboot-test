b9dc3480d37959efb0f5f44af5c4849ad10382fd
adam <adamluthfi6@gmail.com>
2025-03-30 17:16:20 +0700

    add spring boot application

---
+++ Learning/sample-spring/src/test/java/com/sample_spring/core/FooApplicationTest.java
@@ -0,0 +1,20 @@
+package com.sample_spring.core;
+
+import com.sample_spring.core.application.FooApplication;
+import com.sample_spring.core.data.Foo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FooApplication.class)
+public class FooApplicationTest {
+
+    @Autowired
+    Foo foo;
+
+    @Test
+    void testApplication() {
+        Assertions.assertNotNull(foo);
+    }
+}
