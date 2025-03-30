475a25a6de464c4408a2b7afbfa775c13aa83ffc
adam <adamluthfi6@gmail.com>
2025-03-30 17:00:34 +0700

    add Event Listener

---
+++ Learning/sample-spring/src/test/java/com/sample_spring/core/EventListenerTest.java
@@ -0,0 +1,41 @@
+package com.sample_spring.core;
+
+import com.sample_spring.core.data.User;
+import com.sample_spring.core.listener.LoginAgainSuccessListener;
+import com.sample_spring.core.listener.LoginSuccessListener;
+import com.sample_spring.core.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+public class EventListenerTest {
+
+    @Configuration
+    @Import({
+            UserService.class,
+            LoginSuccessListener.class,
+            LoginAgainSuccessListener.class
+    })
+    public static class TestConfiguration {
+
+    }
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(TestConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @Test
+    void testEvent() {
+        UserService userService = applicationContext.getBean(UserService.class);
+        userService.login("adam", "adam");
+        userService.login("adam", "adam");
+        userService.login("lutfi", "salah");
+    }
+}
