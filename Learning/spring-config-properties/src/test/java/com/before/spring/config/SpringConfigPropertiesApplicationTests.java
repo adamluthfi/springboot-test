5fa643853e217501b35f220ee7ff08592562acef
adam <adamluthfi6@gmail.com>
2025-09-12 08:30:58 +0700

    add: new project config properties

---
+++ Learning/spring-config-properties/src/test/java/com/before/spring/config/SpringConfigPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.before.spring.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
